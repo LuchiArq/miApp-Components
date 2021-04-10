7ed6593e5179db3d120d7229a01f488e9894eaad
LuchiArq <lucianoarquiel@gmail.com>
2021-04-10 03:36:48 -0300

    Components and stories buton, heading

---
+++ stories/atoms/Button.stories.js
@@ -0,0 +1,9 @@
+import Button from '../../atoms/Button'
+
+export default {
+    title : 'Atoms/Button',
+    component: Button,
+};
+
+export const Primary = () => <Button type="primary">Primary</Button>
+export const Secundary = () => <Button type="secondary">Secundary</Button>
